281b6a012c3a129daaf5712f1a81ea8fe98c1a03
Marina Gornostaeva <gornostaevamv@gmail.com>
2020-11-28 19:06:26 +0100

    Wrap header content into a link

--- lib/table_of_contents/parser.rb
+++ lib/table_of_contents/parser.rb
@@ -25,9 +25,11 @@ module Jekyll
       def inject_anchors_into_html
         @entries.each do |entry|
           # NOTE: `entry[:id]` is automatically URL encoded by Nokogiri
-          entry[:header_node].wrap(
-            %(<a class="anchor" href="##{entry[:id]}"></a>)
-          )
+          a = Nokogiri::XML::Node.new "a", @doc
+          a['class'] = 'anchor'
+          a['href'] = "##{entry[:id]}"
+          a.children = entry[:header_content_node].children
+          entry[:header_content_node].children = a
         end
 
         @doc.inner_html
@@ -54,7 +56,7 @@ module Jekyll
             text: CGI.escapeHTML(text),
             node_name: node.name,
             header_content: node.children.first,
-            header_node: node,
+            header_content_node: node,
             h_num: node.name.delete('h').to_i
           }
         end
